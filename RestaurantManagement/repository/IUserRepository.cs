25679798eef25116705071e3fcfbd69216066df2
Divesh Singh <divesh.singh@joshtechnologygroup.com>
2026-09-11 15:51:47 +0530

    signup with testcase

---
+++ RestaurantManagement/repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantManagement.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagement.repository
+{
+    public interface IUserRepository
+    {
+        Boolean CheckEmailIsPresent(string email);
+        Boolean CheckPhoneNumberIsPresent(String ph);
+        String AddUser(User userentity);
+    }
+}
